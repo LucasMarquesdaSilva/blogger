32789fb292b7a5f14284430cf686316f4db0523f
LucasMarquesdaSilva <lucas.msilva09@gmail.com>
2016-10-14 13:41:47 -0300

    Adicionando um novo artigo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,22 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 end
